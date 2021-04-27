ca8367b3058b4b184ff825c470dd1ff327dbd17a
htutwaiphyoe <htutwaiphyoe@gmail.com>
2021-04-27 22:24:56 +0630

    #add: 26.4.2021 amount update

--- src/api/homeData.js
+++ src/api/homeData.js
@@ -1,13 +1,13 @@
-export const total = 34_232_562,
-    supported = 19_400_000,
-    balance = 14_832_562,
+export const total = 34_857_562,
+    supported = 19_700_000,
+    balance = 15_157_562,
     percentage = (supported / total) * 100;
 
 export const charts = {
     bar: [
         {
-            labels: ["Apr1", "Apr5", "Apr6", "Apr7", "Apr11", "Apr16", "Apr20", "Apr21", "Apr24"],
-            data: [35_000, 100_000, 5_000, 155_000, 20_000, 50_000, 30_000, 1_000_000, 148_400],
+            labels: ["Apr5", "Apr6", "Apr7", "Apr11", "Apr16", "Apr20", "Apr21", "Apr24", "Apr26"],
+            data: [100_000, 5_000, 155_000, 20_000, 50_000, 30_000, 1_000_000, 148_400, 625_000],
             label: "ငွေပမာဏ",
             title: "နောက်ဆုံး ကောက်ခံရရှိမှု",
             bg: "#9d0208",
@@ -20,12 +20,12 @@ export const charts = {
                 1_467_000,
                 1_883_000,
                 1_469_200,
-                1_555_800,
+                1_680_800,
                 1_445_612,
                 1_230_000,
                 862_550,
                 1_786_000,
-                19_128_400,
+                19_628_400,
             ],
             label: "‌ငွေပမာဏ",
             title: "Batch အလိုက်နှင့် ပြင်ပမှ ကောက်ခံရရှိမှု",
@@ -42,11 +42,11 @@ export const charts = {
     },
 };
 export const donors = [
-    { name: "ပြင်ပအလှူရှင်များ", batch: "N/A", amount: "19,128,400" },
+    { name: "ပြင်ပအလှူရှင်များ", batch: "N/A", amount: "19,628,400" },
     { name: "1st batch ကျောင်းသားဟောင်းများ", batch: "1st", amount: "3,405,000" },
     { name: "Final ကျောင်းသားများ", batch: "3rd", amount: "1,883,000" },
     { name: "ဂျပန်နှင့်နော်ဝေရောက် MTU ကျောင်းသားများ", batch: "N/A", amount: "1,786,000" },
-    { name: "4th year ကျောင်းသားများ", batch: "5th", amount: "1,555,800" },
+    { name: "4th year ကျောင်းသားများ", batch: "5th", amount: "1,680,800" },
     { name: "5th year ကျောင်းသားများ", batch: "4th", amount: "1,469,200" },
     { name: "2nd batch ကျောင်းသားဟောင်းများ", batch: "2nd", amount: "1,467,000" },
     { name: "3rd year ကျောင်းသားများ", batch: "6th", amount: "1,445,612" },
